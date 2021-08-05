8ab214924e1748cc9040a1ac1092b95eddfedd60
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2021-08-05 21:15:00 +0200

    Fix failing build

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from 'next/image'
+
 
 export default function Custom404() {
     return <div>
@@ -10,7 +12,7 @@ export default function Custom404() {
         <main className={'my-16 flex flex-col justify-center text-center'}>
             <div className="text-center">
                 <div className="px-6">
-                    <img src="/images/worker.png" alt="Pixel grafika vzdrževalca" className={'mx-auto max-w-full'}/>
+                    <Image src="/images/worker.png" alt="Pixel grafika vzdrževalca" height={180} width={278}/>
                 </div>
 
                 <div className="mt-12">
