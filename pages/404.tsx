c05c9e71c8a7e5c5b0a7f8b69b4b0a2446d0a79c
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2021-08-05 21:25:11 +0200

    Hardcode the image

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from 'next/image'
-
 
 export default function Custom404() {
     return <div>
@@ -12,7 +10,8 @@ export default function Custom404() {
         <main className={'my-16 flex flex-col justify-center text-center'}>
             <div className="text-center">
                 <div className="px-6">
-                    <Image src="/images/worker.png" alt="Pixel grafika vzdrževalca" height={180} width={278}/>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img src="/images/worker.png" alt="Pixel grafika vzdrževalca"/>
                 </div>
 
                 <div className="mt-12">
