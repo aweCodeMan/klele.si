b7d395d15b6369d44da2cc16bd70ea56cf75649b
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2021-08-13 13:52:08 +0200

    Logout on 401

--- helpers/api-client.ts
+++ helpers/api-client.ts
@@ -1,6 +1,20 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 
-export const ApiClient = axios.create({
+const client = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
 });
+
+client.interceptors.response.use((response: AxiosResponse) => {
+    return response;
+}, (error => {
+    if (error.response.status === 401) {
+        localStorage.clear();
+        window.location.href = window.location.href;
+        return false;
+    }
+
+    return Promise.reject(error);
+}))
+
+export const ApiClient = client;
